2c31527aa8d0a4476227c6ed9209ccb17dd9bba4
007jet <jet00369@hotmail.com>
2015-09-28 18:20:22 -0400

    Create Dialog.h

---
+++ Dialog.h
@@ -0,0 +1,22 @@
+#include <Windows.h>
+#include <stdio.h>
+
+//Control IDS
+#define ID_START 1
+#define ID_STOP 5
+#define ID_ABT 7
+
+#define ID_TRACKBAR 2
+
+#define ID_MESSAGEONE 3
+#define ID_MESSAGETWO 4
+
+#define ID_TRACKVAL 6
+
+//Variables
+extern HWND MessageOne;
+extern HWND MessageTwo;
+extern HWND Button_Start;
+extern HWND Button_Stop;
+extern HGDIOBJ hfDefault;
+extern HWND TESTZ;
